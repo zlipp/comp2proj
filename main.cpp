5412b4cb5beeada1b9f196d4758b6726b483a29f
zlipp <zlipp@nd.edu>
2013-04-09 16:55:10 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,54 @@
+/* Zach Lipp 3/27/13
+Splash Screen driver
+
+reference gpwiki.org for SDL syntax
+ */
+
+#include <SDL/SDL.h>
+#include "splashScreen.h"
+#include <SDL/SDL_image.h>
+#include <iostream>
+#include <SDL/SDL_ttf.h>
+#include <vector>
+using namespace std;
+
+int main(int argc, char* args[]) 
+{
+  //initializes SDL
+  if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+      cout<<"Unable to initialize SDL: "<< SDL_GetError()<<endl;
+      return 1;
+    }
+
+  SDL_Surface *screen;
+
+  //initializes screen
+  screen = SDL_SetVideoMode(640,480,16,SDL_SWSURFACE);
+  if (screen==NULL)
+    {
+      cout<<"Unable to set video mode: "<<SDL_GetError()<<endl;
+      return 1;
+    }
+
+  vector<string> n;
+  n.push_back("Zach");
+  n.push_back("Kaitlin");
+  n.push_back("Kevin");
+
+  vector<int> s;
+  s.push_back(15);
+  s.push_back(12);
+  s.push_back(17);    
+
+  //creates a splashScreen object and displays
+  splashScreen ss;
+  ss.setNames(&n);
+  ss.setScores(&s);
+  ss.display(screen);
+
+
+  SDL_Quit();
+
+  return 0;
+}
